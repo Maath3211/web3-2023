85c797042b20387548b1d30db817824c615c8af4
Maath3211 <mathys.l.lessard@gmail.com>
2023-08-29 15:20:05 -0400

    Création Superchiens

---
+++ SuperChiens/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $db = "chiens";
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $db);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
+    ?>
+
+</body>
+
+</html>
